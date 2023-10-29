c7bb6fb9959c31d6eec2138eeb79a90a2c71da80
Octavio <octabau02@outlook.com>
2023-10-28 19:02:57 -0600

    Añadiendo el estatus al mostrar respuestas

--- foro/src/main/java/com/alura/foro/domain/respuesta/RespuestaResponse.java
+++ foro/src/main/java/com/alura/foro/domain/respuesta/RespuestaResponse.java
@@ -3,10 +3,10 @@ package com.alura.foro.domain.respuesta;
 import java.time.LocalDateTime;
 
 public record RespuestaResponse(Long id, String mensaje, LocalDateTime fechaCreacion, Long autor, Long topico,
-                                Boolean solucion) {
+                                Boolean solucion, Boolean activo) {
     public RespuestaResponse(Respuesta respuesta){
         this(respuesta.getId(), respuesta.getMensaje(), respuesta.getFechaCreacion(), respuesta.getAutor().getId(),
-                respuesta.getTopico().getId(), respuesta.getSolucion());
+                respuesta.getTopico().getId(), respuesta.getSolucion(), respuesta.getActivo());
 
     }
 }
